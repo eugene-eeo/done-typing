cfbb48ff0ae8ed8a3e8af546b2de0fe0c9ae8ba0
Eeo Jun <141bytes@gmail.com>
2016-07-29 16:46:50 +0800

    Add demo page

---
+++ done-typing.js
@@ -0,0 +1,25 @@
+function done_typing(config) {
+  var before = config.before || function() {};
+  var after  = config.after  || function() {};
+  var delay  = config.delay  || 200;
+
+  var stopped = true;
+  var timeout = null;
+
+  return function() {
+    var ctx  = this;
+    var args = arguments;
+
+    stopped = false;
+    if (timeout === null) {
+      before.apply(ctx, args);
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      stopped = true;
+      timeout = null;
+      after.apply(ctx, args);
+    }, delay);
+  };
+};
